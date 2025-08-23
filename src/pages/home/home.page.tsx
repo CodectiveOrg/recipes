414bbec7a72174d6407598cfcf4e5ea6f8f19ee4
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-23 07:38:44 +0330

    [ADD] hello link to home page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,7 +1,15 @@
 import type { ReactNode } from "react";
 
+import { Link } from "react-router";
+
 import styles from "./home.module.css";
 
 export default function HomePage(): ReactNode {
-  return <div className={styles.home}>Hello, friend!</div>;
+  return (
+    <div className={styles.home}>
+      Hello, friend!
+      <br />
+      <Link to="/hello">Hello!</Link>
+    </div>
+  );
 }
