a400d82ca213d0afafbac72b426804c4206a0d71
ZahraZare <zahrazare3099@gmail.com>
2025-08-26 12:55:08 +0330

    fix bug in style

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,4 +1,11 @@
-import type { ComponentProps, ReactNode } from "react";
+import {
+  type ButtonHTMLAttributes,
+  type ComponentProps,
+  type JSX,
+  type MouseEvent,
+  useRef,
+  useState,
+} from "react";
 
 import clsx from "clsx";
 
@@ -8,6 +15,11 @@ type Props = ComponentProps<"button"> & {
   className?: string;
   variant?: "contained" | "outlined" | "text";
   fullwidth?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+type Ripple = {
+  key: number;
+  style: React.CSSProperties;
 };
 
 export default function ButtonComponent({
@@ -16,7 +28,43 @@ export default function ButtonComponent({
   fullwidth = false,
   children,
   ...otherProps
-}: Props): ReactNode {
+}: Props): JSX.Element {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [ripples, setRipples] = useState<Ripple[]>([]);
+
+  const createRipple = (event: MouseEvent<HTMLButtonElement>): void => {
+    const button = buttonRef.current;
+
+    if (!button) return;
+
+    setRipples([]);
+
+    const rect = button.getBoundingClientRect();
+
+    const diameter = Math.max(rect.width, rect.height);
+    const radius = diameter / 2;
+
+    let left = event.clientX - rect.left - radius;
+    let top = event.clientY - rect.top - radius;
+
+    const newRipple: Ripple = {
+      key: Date.now(),
+      style: {
+        width: diameter + "px",
+        height: diameter + "px",
+        left: left + "px",
+        top: top + "px",
+        position: "absolute",
+      },
+    };
+
+    setRipples([newRipple]);
+
+    setTimeout(() => {
+      setRipples([]);
+    }, 1500);
+  };
+
   return (
     <button
       className={clsx(
@@ -25,9 +73,14 @@ export default function ButtonComponent({
         fullwidth && styles.fullwidth,
         className,
       )}
+      ref={buttonRef}
+      onMouseDown={createRipple}
       {...otherProps}
     >
       {children}
+      {ripples.map((ripple) => (
+        <span key={ripple.key} className={styles.ripple} style={ripple.style} />
+      ))}
     </button>
   );
 }
