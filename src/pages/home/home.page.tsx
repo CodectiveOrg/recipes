dfdf777098d8ed9375b9edb47bcdcd74391eb232
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-26 09:20:27 +0330

    [REMOVE] test page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,31 +1,15 @@
-import { type ReactNode } from "react";
+import type { ReactNode } from "react";
 
-import TextInputComponent from "@/components/text-input/text-input.component.tsx";
+import { Link } from "react-router";
 
 import styles from "./home.module.css";
 
 export default function HomePage(): ReactNode {
   return (
     <div className={styles.home}>
-      <TextInputComponent label="Label" placeholder="Placeholder text" />
-      <TextInputComponent
-        label="Label"
-        placeholder="Placeholder text"
-        disabled
-      />
-      <TextInputComponent
-        label="Label"
-        placeholder="Placeholder text"
-        error="This is error text"
-      />
-      <TextInputComponent fullwidth label="Label" value="Typed text" />
-      <TextInputComponent fullwidth label="Label" value="Typed text" disabled />
-      <TextInputComponent
-        fullwidth
-        label="Label"
-        value="Typed text"
-        error="This is error text"
-      />
+      Hello, friend!
+      <br />
+      <Link to="/hello">Hello!</Link>
     </div>
   );
 }
