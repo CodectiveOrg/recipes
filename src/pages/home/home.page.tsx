4235a15dff5f18da2a053d8c186b27e310ccc93a
ZahraZare <zahrazare3099@gmail.com>
2025-08-30 16:05:25 +0330

    display button components

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -9,59 +9,35 @@ export default function HomePage(): ReactNode {
     <div className={styles.home}>
       <header>Header</header>
       <main>
-        <ButtonComponent>Button</ButtonComponent>
-        <ButtonComponent size="medium">Button</ButtonComponent>
-        <ButtonComponent size="small">Button</ButtonComponent>
-        {/*  */}
-        <ButtonComponent variant="outline">Button</ButtonComponent>
-        <ButtonComponent variant="outline" size="medium">
-          Button
-        </ButtonComponent>
-        <ButtonComponent variant="outline" size="small">
-          Button
-        </ButtonComponent>
-        {/*  */}
-        <ButtonComponent variant="text">Button</ButtonComponent>
-        <ButtonComponent variant="text" size="medium">
-          Button
-        </ButtonComponent>
-        <ButtonComponent variant="text" size="small">
-          Button
-        </ButtonComponent>
-        {/* destructive */}
-        <ButtonComponent color="destructive">Button</ButtonComponent>
-        <ButtonComponent color="destructive" size="medium">
-          Button
-        </ButtonComponent>
-        <ButtonComponent color="destructive" size="small">
-          Button
-        </ButtonComponent>
-        {/*  */}
-        <ButtonComponent color="destructive" variant="outline">
-          Button
-        </ButtonComponent>
-        <ButtonComponent color="destructive" variant="outline" size="medium">
-          Button
-        </ButtonComponent>
-        <ButtonComponent color="destructive" variant="outline" size="small">
-          Button
-        </ButtonComponent>
-        {/*  */}
-        <ButtonComponent color="destructive" variant="text">
-          Button
-        </ButtonComponent>
-        <ButtonComponent color="destructive" variant="text" size="medium">
-          Button
-        </ButtonComponent>
-        <ButtonComponent color="destructive" variant="text" size="small">
-          Button
-        </ButtonComponent>
-        {/* secondary */}
         <ButtonComponent color="secondary">Button</ButtonComponent>
         <ButtonComponent color="secondary" size="medium">
           Button
         </ButtonComponent>
         <ButtonComponent color="secondary" size="small">
+          Button
+        </ButtonComponent>
+        <ButtonComponent
+          color="secondary"
+          size="default-with-icon"
+          withIcon
+          iconName="square-academic-cap-2-bold"
+        >
+          Button
+        </ButtonComponent>
+        <ButtonComponent
+          color="secondary"
+          withIcon
+          iconName="square-academic-cap-2-bold"
+          size="medium-with-icon"
+        >
+          Button
+        </ButtonComponent>
+        <ButtonComponent
+          color="secondary"
+          withIcon
+          iconName="square-academic-cap-2-bold"
+          size="small-with-icon"
+        >
           Button
         </ButtonComponent>
         {/*  */}
@@ -74,6 +50,33 @@ export default function HomePage(): ReactNode {
         <ButtonComponent color="secondary" variant="outline" size="small">
           Button
         </ButtonComponent>
+        <ButtonComponent
+          color="secondary"
+          size="default-with-icon"
+          withIcon
+          variant="outline"
+          iconName="square-academic-cap-2-bold"
+        >
+          Button
+        </ButtonComponent>
+        <ButtonComponent
+          color="secondary"
+          withIcon
+          variant="outline"
+          iconName="square-academic-cap-2-bold"
+          size="medium-with-icon"
+        >
+          Button
+        </ButtonComponent>
+        <ButtonComponent
+          color="secondary"
+          withIcon
+          variant="outline"
+          iconName="square-academic-cap-2-bold"
+          size="small-with-icon"
+        >
+          Button
+        </ButtonComponent>
         {/*  */}
         <ButtonComponent color="secondary" variant="text">
           Button
@@ -84,32 +87,31 @@ export default function HomePage(): ReactNode {
         <ButtonComponent color="secondary" variant="text" size="small">
           Button
         </ButtonComponent>
-        {/* disable */}
-        <ButtonComponent color="disable">Button</ButtonComponent>
-        <ButtonComponent color="disable" size="medium">
+        <ButtonComponent
+          color="secondary"
+          variant="text"
+          withIcon
+          size="default-with-icon"
+          iconName="square-academic-cap-2-bold"
+        >
           Button
         </ButtonComponent>
-        <ButtonComponent color="disable" size="small">
+        <ButtonComponent
+          color="secondary"
+          variant="text"
+          withIcon
+          iconName="square-academic-cap-2-bold"
+          size="medium-with-icon"
+        >
           Button
         </ButtonComponent>
-        {/*  */}
-        <ButtonComponent color="disable" variant="outline">
-          Button
-        </ButtonComponent>
-        <ButtonComponent color="disable" variant="outline" size="medium">
-          Button
-        </ButtonComponent>
-        <ButtonComponent color="disable" variant="outline" size="small">
-          Button
-        </ButtonComponent>
-        {/*  */}
-        <ButtonComponent color="disable" variant="text">
-          Button
-        </ButtonComponent>
-        <ButtonComponent color="disable" variant="text" size="medium">
-          Button
-        </ButtonComponent>
-        <ButtonComponent color="disable" variant="text" size="small">
+        <ButtonComponent
+          color="secondary"
+          variant="text"
+          withIcon
+          iconName="square-academic-cap-2-bold"
+          size="small-with-icon"
+        >
           Button
         </ButtonComponent>
       </main>
