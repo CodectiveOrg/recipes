b4c3f7fbdaa30f84f55e9ba789683e1eb3575f91
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-30 09:43:18 +0330

    [REFACTOR] inline type

--- src/components/typography/typography.component.tsx
+++ src/components/typography/typography.component.tsx
@@ -4,17 +4,13 @@ import clsx from "clsx";
 
 import styles from "./typography.module.css";
 
-type VariantType = "h1" | "h2" | "h3" | "p1" | "p2" | "s";
-
-type ColorType = "text" | "text-secondary";
-
 type Props = PropsWithChildren<
   ({ p?: boolean; span?: never } | { p?: never; span?: boolean }) & {
     ellipsis?: boolean;
     className?: string;
     style?: CSSProperties;
-    variant: VariantType;
-    color?: ColorType;
+    variant: "h1" | "h2" | "h3" | "p1" | "p2" | "s";
+    color?: "text" | "text-secondary";
     maxLines?: number;
   }
 >;
