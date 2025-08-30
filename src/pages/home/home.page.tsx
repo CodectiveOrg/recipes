5f6e7e31f14f20866811a604f4c585de32e8cc52
Alireza Hassani Eghtedar <alirezaprogrammer2001@gmail.com>
2025-08-30 11:35:21 +0330

    [ADD] temp nav items

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -3,13 +3,48 @@ import type { ReactNode } from "react";
 import { Link } from "react-router";
 
 import IconComponent from "@/components/icon/icon.component.tsx";
+import NavbarComponent from "@/components/navbar/navbar.component.tsx";
 import TypographyComponent from "@/components/typography/typography.component.tsx";
 
 import styles from "./home.module.css";
 
+type NavItem = {
+  icon: ReactNode;
+  title: string;
+  href: string;
+};
+const navItems: NavItem[] = [
+  {
+    icon: <IconComponent name="home-angle-2-bold" />,
+    title: "Home",
+    href: "/",
+  },
+  {
+    icon: <IconComponent name="magnifer-linear" />,
+    title: "Search",
+    href: "/search",
+  },
+  {
+    icon: <IconComponent name="add-circle-broken" />,
+    title: "Add",
+    href: "/add",
+  },
+  {
+    icon: <IconComponent name="bell-bold" />,
+    title: "Notification",
+    href: "/notification",
+  },
+  {
+    icon: <IconComponent name="user-bold" />,
+    title: "Profile",
+    href: "/profile",
+  },
+];
+
 export default function HomePage(): ReactNode {
   return (
     <div className={styles.home}>
+      <NavbarComponent navbarItems={navItems} />
       <TypographyComponent variant="h1">Heading 1</TypographyComponent>
       <TypographyComponent variant="h2">Heading 2</TypographyComponent>
       <TypographyComponent variant="h3">Heading 3</TypographyComponent>
