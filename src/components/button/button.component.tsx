4f351bab2a371b4e1c8e08ba46e3cdce6f477805
ZahraZare <zahrazare3099@gmail.com>
2025-08-30 16:04:59 +0330

    [ADD] icon for button coponent

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -2,20 +2,32 @@ import { type ComponentProps, type ReactNode } from "react";
 
 import clsx from "clsx";
 
+import IconComponent from "../icon/icon.component";
+
 import styles from "./button.module.css";
 
 type Props = ComponentProps<"button"> & {
   className?: string;
   color?: "primary" | "secondary" | "destructive" | "disable";
-  variant?: "default" | "outline" | "text";
-  size?: "large" | "medium" | "small";
+  variant?: "fill" | "outline" | "text";
+  size?:
+    | "default"
+    | "medium"
+    | "small"
+    | "default-with-icon"
+    | "medium-with-icon"
+    | "small-with-icon";
+  withIcon?: boolean;
+  iconName?: string;
 };
 
 export default function ButtonComponent({
   className,
-  variant = "default",
+  variant = "fill",
   color = "primary",
-  size = "large",
+  size = "default",
+  withIcon = false,
+  iconName = "",
   children,
   ...otherProps
 }: Props): ReactNode {
@@ -30,7 +42,7 @@ export default function ButtonComponent({
       )}
       {...otherProps}
     >
-      {/* {isIcon && <IconComponent name={iconName} className={styles["icon"]} />}{" "} */}
+      {withIcon && <IconComponent name={iconName} className={styles["icon"]} />}
       {children}
     </button>
   );
