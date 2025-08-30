343c8c6e13ccc0e5748da76b5f38b85ee1db735a
ZahraZare <zahrazare3099@gmail.com>
2025-08-30 11:44:00 +0330

    display button component

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -7,34 +7,107 @@ import styles from "./home.module.css";
 export default function HomePage(): ReactNode {
   return (
     <div className={styles.home}>
-      <ButtonComponent variant="contained">Button</ButtonComponent>
-      <ButtonComponent variant="contained" disabled>
+      <ButtonComponent>Button</ButtonComponent>
+      <ButtonComponent size="medium">Button</ButtonComponent>
+      <ButtonComponent size="small">Button</ButtonComponent>
+      {/*  */}
+      <ButtonComponent variant="outline">Button</ButtonComponent>
+      <ButtonComponent variant="outline" size="medium">
         Button
       </ButtonComponent>
-      <ButtonComponent variant="outlined">Button</ButtonComponent>
-      <ButtonComponent variant="outlined" disabled>
+      <ButtonComponent variant="outline" size="small">
         Button
       </ButtonComponent>
+      {/*  */}
       <ButtonComponent variant="text">Button</ButtonComponent>
-      <ButtonComponent variant="text" disabled>
+      <ButtonComponent variant="text" size="medium">
         Button
       </ButtonComponent>
-      <ButtonComponent fullwidth variant="contained">
+      <ButtonComponent variant="text" size="small">
         Button
       </ButtonComponent>
-      <ButtonComponent fullwidth variant="contained" disabled>
+      {/* destructive */}
+      <ButtonComponent color="destructive">Button</ButtonComponent>
+      <ButtonComponent color="destructive" size="medium">
         Button
       </ButtonComponent>
-      <ButtonComponent fullwidth variant="outlined">
+      <ButtonComponent color="destructive" size="small">
         Button
       </ButtonComponent>
-      <ButtonComponent fullwidth variant="outlined" disabled>
+      {/*  */}
+      <ButtonComponent color="destructive" variant="outline">
         Button
       </ButtonComponent>
-      <ButtonComponent fullwidth variant="text">
+      <ButtonComponent color="destructive" variant="outline" size="medium">
         Button
       </ButtonComponent>
-      <ButtonComponent fullwidth variant="text" disabled>
+      <ButtonComponent color="destructive" variant="outline" size="small">
+        Button
+      </ButtonComponent>
+      {/*  */}
+      <ButtonComponent color="destructive" variant="text">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="destructive" variant="text" size="medium">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="destructive" variant="text" size="small">
+        Button
+      </ButtonComponent>
+      {/* secondary */}
+      <ButtonComponent color="secondary">Button</ButtonComponent>
+      <ButtonComponent color="secondary" size="medium">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="secondary" size="small">
+        Button
+      </ButtonComponent>
+      {/*  */}
+      <ButtonComponent color="secondary" variant="outline">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="secondary" variant="outline" size="medium">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="secondary" variant="outline" size="small">
+        Button
+      </ButtonComponent>
+      {/*  */}
+      <ButtonComponent color="secondary" variant="text">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="secondary" variant="text" size="medium">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="secondary" variant="text" size="small">
+        Button
+      </ButtonComponent>
+      {/* disable */}
+      <ButtonComponent color="disable">Button</ButtonComponent>
+      <ButtonComponent color="disable" size="medium">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="disable" size="small">
+        Button
+      </ButtonComponent>
+      {/*  */}
+      <ButtonComponent color="disable" variant="outline">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="disable" variant="outline" size="medium">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="disable" variant="outline" size="small">
+        Button
+      </ButtonComponent>
+      {/*  */}
+      <ButtonComponent color="disable" variant="text">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="disable" variant="text" size="medium">
+        Button
+      </ButtonComponent>
+      <ButtonComponent color="disable" variant="text" size="small">
         Button
       </ButtonComponent>
     </div>
