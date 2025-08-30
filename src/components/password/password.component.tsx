c594e5cc741c439c5d889a6d31820eb29c38e25e
mazaherireza <rezamazaheri.email@gmail.com>
2025-08-30 14:44:39 +0330

    [FIX] end tag

--- src/components/password/password.component.tsx
+++ src/components/password/password.component.tsx
@@ -4,5 +4,5 @@ import TextInputComponent from "@/components/text-input/text-input.component";
 import IconComponent from "@/components/icon/icon.component";
 
 export default function PasswordComponent(): ReactNode {
-    return <TextInputComponent type="password" startAdornment={<IconComponent name="solar:lock-keyhole-minimalistic-bold" color="primary" />} endAdornment={<IconComponent name="solar:eye-outline" color="primary" />>} />
+    return <TextInputComponent type="password" startAdornment={<IconComponent name="solar:lock-keyhole-minimalistic-bold" color="primary" />} endAdornment={<IconComponent name="solar:eye-outline" color="primary" />} />
 }
