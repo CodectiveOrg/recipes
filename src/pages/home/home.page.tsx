be5d0674adf85c007c478d0977cba01432ee5f2c
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-31 09:01:04 +0330

    [REVERT] home page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,7 +1,9 @@
 import type { ReactNode } from "react";
 
-import ButtonComponent from "@/components/button/button.component.tsx";
+import { Link } from "react-router";
+
 import IconComponent from "@/components/icon/icon.component.tsx";
+import TypographyComponent from "@/components/typography/typography.component.tsx";
 
 import styles from "./home.module.css";
 
@@ -10,26 +12,82 @@ export default function HomePage(): ReactNode {
     <div className={styles.home}>
       <header>Header</header>
       <main>
-        {["primary", "secondary", "danger", "disabled"].flatMap((color) =>
-          ["solid", "outlined", "text"].flatMap((variant) =>
-            [false, true].flatMap((icon, iconIndex) => (
-              <div key={iconIndex} className={styles.row}>
-                {["large", "medium", "small"].map((size, sizeIndex) => (
-                  <ButtonComponent
-                    key={sizeIndex}
-                    variant={variant}
-                    color={color === "disabled" ? "primary" : color}
-                    size={size}
-                    disabled={color === "disabled"}
-                  >
-                    {icon && <IconComponent name="widget-linear" />}
-                    Button
-                  </ButtonComponent>
-                ))}
-              </div>
-            )),
-          ),
-        )}
+        <TypographyComponent variant="h1">Heading 1</TypographyComponent>
+        <TypographyComponent variant="h2">Heading 2</TypographyComponent>
+        <TypographyComponent variant="h3">Heading 3</TypographyComponent>
+        <TypographyComponent variant="p1">Paragraph 1</TypographyComponent>
+        <TypographyComponent variant="p2">Paragraph 2</TypographyComponent>
+        <TypographyComponent variant="s">Small</TypographyComponent>
+        <br />
+        <TypographyComponent variant="h1" color="text-secondary">
+          Heading 1
+        </TypographyComponent>
+        <TypographyComponent variant="h2" color="text-secondary">
+          Heading 2
+        </TypographyComponent>
+        <TypographyComponent variant="h3" color="text-secondary">
+          Heading 3
+        </TypographyComponent>
+        <TypographyComponent variant="p1" color="text-secondary">
+          Paragraph 1
+        </TypographyComponent>
+        <TypographyComponent variant="p2" color="text-secondary">
+          Paragraph 2
+        </TypographyComponent>
+        <TypographyComponent variant="s" color="text-secondary">
+          Small
+        </TypographyComponent>
+        <br />
+        <TypographyComponent
+          p
+          ellipsis
+          variant="p1"
+          style={{ maxInlineSize: "40ch" }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+          accusantium architecto doloremque dolorum explicabo, impedit incidunt,
+          ipsa reprehenderit saepe tempore tenetur vel.
+        </TypographyComponent>
+        <br />
+        <TypographyComponent
+          p
+          variant="p2"
+          maxLines={3}
+          style={{ maxInlineSize: "40ch" }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+          accusantium architecto doloremque dolorum explicabo, impedit incidunt,
+          ipsa reprehenderit saepe tempore tenetur vel.
+        </TypographyComponent>
+        <br />
+        <div style={{ fontSize: "2rem" }}>
+          <IconComponent name="home-smile-angle-bold" color="primary" />
+          <IconComponent name="user-linear" color="primary" />
+          <IconComponent name="settings-linear" color="primary" />
+          <IconComponent name="eye-closed-linear" color="text" />
+          <IconComponent
+            name="lock-keyhole-minimalistic-unlocked-linear"
+            color="text-secondary"
+          />
+          <IconComponent name="trash-bin-trash-linear" color="danger" />
+        </div>
+        <br />
+        <Link to="/hello">Call Hello API</Link>
+        <br />
+        <TypographyComponent p variant="p2" style={{ maxInlineSize: "40ch" }}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+          atque autem consectetur dolorem eaque enim ex harum hic id illo labore
+          libero magni, non obcaecati quibusdam sequi similique vitae
+          voluptatem. Corporis est illum, natus porro quidem quos? Accusantium
+          eos fugiat maiores nobis perferendis voluptatibus? A aperiam delectus,
+          ea eum fuga harum illum, iste odit optio, sint unde vel voluptas
+          voluptatibus. Accusantium consequuntur delectus et nostrum, officia
+          sit unde! Ab aspernatur beatae consequuntur corporis delectus dicta
+          distinctio dolore dolores eligendi enim exercitationem ipsam magni
+          nesciunt nisi nobis omnis, praesentium provident quasi quo quos
+          repellendus saepe tempore ullam velit voluptatibus. Dolorum,
+          praesentium!
+        </TypographyComponent>
       </main>
     </div>
   );
