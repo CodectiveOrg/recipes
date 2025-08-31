3e21c453a7cd12cf16e681ff5f955d3df1a8bd7b
ZahraZare <zahrazare3099@gmail.com>
2025-08-31 21:40:58 +0330

    Implement range input component

--- src/components/range-input/range-input.component.tsx
+++ src/components/range-input/range-input.component.tsx
@@ -1,5 +1,57 @@
+import { type ComponentProps, type ReactNode } from "react";
+
+import clsx from "clsx";
+
+import TypographyComponent from "../typography/typography.component";
+
 import styles from "./range-input.module.css";
 
-export default function RangeInputComponent() {
-  return <div className={styles["range-input-component"]}></div>;
+type Props = ComponentProps<"input"> & {
+  label: string;
+};
+export default function RangeInputComponent({
+  className,
+  label,
+  ...otherProps
+}: Props): ReactNode {
+  return (
+    <div className={clsx(styles["range-input-component"], className)}>
+      <label className={styles.title} htmlFor={label}>
+        <TypographyComponent variant="h2" color="text">
+          {label}
+        </TypographyComponent>
+        <TypographyComponent variant="p1" color="text-secondary">
+          (in minutes)
+        </TypographyComponent>
+      </label>
+      <div className={styles["range-input-container"]}>
+        <div className={styles["range-labels"]}>
+          <TypographyComponent variant="h3" className={styles["range-label"]}>
+            &lt;10
+          </TypographyComponent>
+          <TypographyComponent variant="h3" className={styles["range-label"]}>
+            35
+          </TypographyComponent>
+          <TypographyComponent variant="h3" className={styles["range-label"]}>
+            &gt;60
+          </TypographyComponent>
+        </div>
+        <input className={styles["input-range-slider"]} {...otherProps} />
+      </div>
+    </div>
+  );
 }
+
+/**
+ * usage:
+ *  <RangeInputComponent
+          type="range"
+          min={10}
+          max={60}
+          value={value}
+          onChange={(e)=>setValue(Number(e.target.value))}
+          label="Cooking Duration"
+          style={{ "--fill-percentage": `${percentage}%` } as CSSProperties}
+        />
+     const percentage = ((value - 10) / (60 - 10)) * 100;
+ */
