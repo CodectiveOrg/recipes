a112676e2a5f13fe12479818f8fd21e0df575320
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-01 18:17:49 +0330

    [ADD] styles

--- src/pages/onboarding/onboarding.page.tsx
+++ src/pages/onboarding/onboarding.page.tsx
@@ -1,20 +1,35 @@
 import type { ReactNode } from "react";
 
+import { useNavigate } from "react-router";
+
+import image from "@/assets/images/foods.png";
+
 import ButtonComponent from "@/components/button/button.component";
 import TypographyComponent from "@/components/typography/typography.component";
 
 import styles from "./onboarding.module.css";
 
 export default function OnboardingPage(): ReactNode {
+  const navigate = useNavigate();
+
+  const handleGetStartedButtonClick = () => {
+    navigate("/login");
+  };
+
   return (
     <div className={styles.onboarding}>
       <header></header>
       <main>
-        <TypographyComponent variant="h1">Start Cooking</TypographyComponent>
-        <TypographyComponent p variant="p1" color="text-secondary">
-          Let’s join our community to cook better food!
-        </TypographyComponent>
-        <ButtonComponent>Get Started</ButtonComponent>
+        <img src={image} alt="" />
+        <div className={styles.writings}>
+          <TypographyComponent variant="h1">Start Cooking</TypographyComponent>
+          <TypographyComponent p variant="p1" color="text-secondary">
+            Let’s join our community to cook better food!
+          </TypographyComponent>
+        </div>
+        <ButtonComponent onClick={handleGetStartedButtonClick}>
+          Get Started
+        </ButtonComponent>
       </main>
     </div>
   );
