ae3caabe38b62a0a0eaa23dc6e6abbf9c6c1e3c8
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-01 10:45:24 +0330

    [ADD] use with ref

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,15 +1,27 @@
-import { type ReactNode } from "react";
+import { type FormEvent, type ReactNode, useRef } from "react";
 
+import ButtonComponent from "@/components/button/button.component.tsx";
 import UploadImageComponent from "@/components/upload-image/upload-image.component";
 
 import styles from "./home.module.css";
 
 export default function HomePage(): ReactNode {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    console.log(ref.current?.value);
+  };
+
   return (
     <div className={styles.home}>
       <header>Header</header>
       <main>
-        <UploadImageComponent />
+        <form onSubmit={handleFormSubmit}>
+          <UploadImageComponent ref={ref} />
+          <ButtonComponent>Submit</ButtonComponent>
+        </form>
       </main>
     </div>
   );
