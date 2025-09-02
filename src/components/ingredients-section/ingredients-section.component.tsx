5c11a00f47061e44e355a4ed858d4397219b6b51
ZahraZare <zahrazare3099@gmail.com>
2025-09-02 18:24:34 +0330

    Implement component

--- src/components/ingredients-section/ingredients-section.component.tsx
+++ src/components/ingredients-section/ingredients-section.component.tsx
@@ -1,5 +1,36 @@
 import type { ReactNode } from "react";
 
-export default function IngredientsSectionComponent(): ReactNode {
-  return <div></div>;
+import IconComponent from "../icon/icon.component";
+import TypographyComponent from "../typography/typography.component";
+
+import styles from "./ingredients-section.module.css";
+
+type Ingredient = {
+  title: string;
+  quantity: string;
+};
+
+type Props = {
+  ingredients: Ingredient[];
+};
+
+export default function IngredientsSectionComponent({
+  ingredients,
+}: Props): ReactNode {
+  return (
+    <div className={styles["ingredients-section"]}>
+      <TypographyComponent variant="h2">Ingredients</TypographyComponent>
+      <ul>
+        {ingredients.map((i, index) => (
+          <li key={index}>
+            <IconComponent name="check-circle-bold" color="primary" />
+            <TypographyComponent span variant="p2">
+              {i.quantity}&nbsp;
+              {i.title}
+            </TypographyComponent>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
