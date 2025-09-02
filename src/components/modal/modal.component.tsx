16f07c5cf3bd328ba24e4157dcfbfc6d9eb1732d
mazaherireza <rezamazaheri.email@gmail.com>
2025-09-02 13:59:25 +0330

    [UPDATE]

--- src/components/modal/modal.component.tsx
+++ src/components/modal/modal.component.tsx
@@ -1,5 +1,4 @@
 import type {
-  CSSProperties,
   MouseEvent,
   PropsWithChildren,
   ReactNode,
@@ -8,27 +7,18 @@ import type {
 
 import clsx from "clsx";
 
-import TypographyComponent from "@/components/typography/typography.component";
-
 import styles from "./modal.module.css";
 
 type Props = PropsWithChildren<{
   ref: RefObject<HTMLDialogElement | null>;
   className?: string;
-  contentClassName?: string;
-  bodyClassName?: string;
   title: string;
-  maxInlineSize?: CSSProperties["inlineSize"];
   onClose: () => void;
 }>;
 
 export default function ModalComponent({
   ref,
   className,
-  contentClassName,
-  bodyClassName,
-  title,
-  maxInlineSize = "35rem",
   onClose,
   children,
 }: Props): ReactNode {
@@ -46,16 +36,10 @@ export default function ModalComponent({
     <dialog
       ref={ref}
       className={clsx(styles.modal, className)}
-      style={{ inlineSize: `min(${maxInlineSize}, 100%)` }}
       onClick={handleClick}
       onClose={onClose}
     >
-      <div className={clsx(styles.content, contentClassName)}>
-        <header>
-          <TypographyComponent variant="h1">{title}</TypographyComponent>
-        </header>
-        <div className={clsx(styles.body, bodyClassName)}>{children}</div>
-      </div>
+      {children}
     </dialog>
   );
 }
