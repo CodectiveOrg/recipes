b3d7042f55a50a3106194b88b5b6bfb21c17de87
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-02 11:06:12 +0330

    [ADD] dynamic size based on props

--- src/components/text-area/text-area.component.tsx
+++ src/components/text-area/text-area.component.tsx
@@ -1,18 +1,36 @@
-import type { ComponentProps, ReactNode } from "react";
+import type { CSSProperties, ComponentProps, ReactNode } from "react";
 
 import clsx from "clsx";
 
 import styles from "./text-area.module.css";
 
-type Props = ComponentProps<"textarea">;
+type Props = ComponentProps<"textarea"> & {
+  defaultLines?: number;
+  minLines?: number;
+  maxLines?: number;
+};
 
 export default function TextAreaComponent({
   className,
+  style,
+  defaultLines = 3,
+  minLines = 1,
+  maxLines = 5,
   ...otherProps
 }: Props): ReactNode {
+  const customStyles = {
+    "--default-lines": `${defaultLines}`,
+    "--min-lines": `${minLines}`,
+    "--max-lines": `${maxLines}`,
+  } as CSSProperties;
+
   return (
     <textarea
-      className={clsx(styles["text-area"], "s", className)}
+      className={clsx(styles["text-area"], className)}
+      style={{
+        ...customStyles,
+        ...style,
+      }}
       {...otherProps}
     ></textarea>
   );
