7585f526f26d1f007e915aabd49058d2d0f56ee3
ZahraZare <zahrazare3099@gmail.com>
2025-09-02 12:19:25 +0330

    display component

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -10,6 +10,7 @@ import ImageInputComponent from "@/components/image-input/image-input.component.
 import PasswordInputComponent from "@/components/password-input/password-input.component.tsx";
 import RangeInputComponent from "@/components/range-input/range-input.component";
 import SearchInputComponent from "@/components/search-input/search-input.component.tsx";
+import SelectedTabsComponent from "@/components/selected-tabs/selected-tabs.component";
 import TextAreaComponent from "@/components/text-area/text-area.component.tsx";
 import TextInputComponent from "@/components/text-input/text-input.component.tsx";
 import TypographyComponent from "@/components/typography/typography.component.tsx";
@@ -24,6 +25,9 @@ export default function HomePage(): ReactNode {
     <div className={styles.home}>
       <header>Header</header>
       <main>
+        <br />
+        <SelectedTabsComponent />
+        <br />
         <TextAreaComponent />
         <br />
         <TextAreaComponent
