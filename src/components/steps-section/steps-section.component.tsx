6b1e1fd70aabe6cfdf9cca9f5562630b75154628
ZahraZare <zahrazare3099@gmail.com>
2025-09-02 19:24:07 +0330

    Implement component

--- src/components/steps-section/steps-section.component.tsx
+++ src/components/steps-section/steps-section.component.tsx
@@ -1,7 +1,35 @@
 import type { ReactNode } from "react";
 
+import TypographyComponent from "../typography/typography.component";
+
 import styles from "./steps-section.module.css";
 
-export default function StepsSectionComponent(): ReactNode {
-  return <div className={styles["steps-section"]}></div>;
+type Step = {
+  description: string;
+  image: string;
+};
+
+type Props = {
+  steps: Step[];
+};
+
+export default function StepsSectionComponent({ steps }: Props): ReactNode {
+  return (
+    <div className={styles["steps-section"]}>
+      <TypographyComponent variant="h2">Steps</TypographyComponent>
+      {steps.map((step, index) => (
+        <div key={index} className={styles.step}>
+          <TypographyComponent variant="s" className={styles.counter}>
+            {index + 1}
+          </TypographyComponent>
+          <div className={styles.description}>
+            <TypographyComponent span variant="p2">
+              {step.description}
+            </TypographyComponent>
+            {step.image ? <img src={step.image} alt="" /> : null}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
