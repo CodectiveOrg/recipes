95dfe2bf5729b8aef5c0bad64a905030e203af1a
Reza Mazaheri <163832802+mazaherireza@users.noreply.github.com>
2025-09-03 15:01:18 +0330

    43 Authentication (#21)
    * [ADD] onboarding
    
    * [ADD] styles
    
    * [ADD] sign-in page
    
    * [ADD] sign in
    
    * [FIX] lint
    
    * [REFACTOR] routing
    
    * [ADD] loading component
    
    * [RENAME] login to sign in
    
    * [REFACTOR] onboarding
    
    * [ADD] link button component
    
    * [REFACTOR] button components
    
    * [REFACTOR] button component
    
    * [FIX] button position
    
    * [ADD] guest layout
    
    * [RENAME] filename to page
    
    * [REFACTOR] styles
    
    * [SYNC] sign up with sign in
    
    * [REFACTOR] shared logic and styles to a single component
    
    * [REFACTOR] validation
    
    * [ADD] auth validation component
    
    * [ADD] use auth validation hook
    
    * [ADD] password auto complete
    
    * [ADD] api connection
    
    ---------
    
    Co-authored-by: BijanProgrammer <bijaneisapour@gmail.com>

--- src/api/auth/sign-up.api.ts
+++ src/api/auth/sign-up.api.ts
@@ -1,9 +1,9 @@
-import type { SignUpRequestDto } from "@/dto/request/sign-up.request.dto.ts";
+import type { AuthRequestDto } from "@/dto/request/auth.request.dto.ts";
 import type { ResponseDto } from "@/dto/response/response.dto.ts";
 
 import { richFetch } from "@/utils/fetch.utils.ts";
 
-export async function signUpApi(dto: SignUpRequestDto): Promise<ResponseDto> {
+export async function signUpApi(dto: AuthRequestDto): Promise<ResponseDto> {
   return await richFetch("/auth/sign-up", {
     method: "POST",
     body: JSON.stringify(dto),
