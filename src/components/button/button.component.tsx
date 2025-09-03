a8f9462905d59a05c8ca29c20f4b03ae0c9724bb
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-03 11:17:18 +0330

    [ADD] link button component

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -1,35 +1,66 @@
 import { type ComponentProps, type ReactNode } from "react";
 
+import { Link } from "react-router";
+
 import clsx from "clsx";
 
 import styles from "./button.module.css";
 
-type Props = ComponentProps<"button"> & {
+type SharedProps = {
+  className?: string;
   variant?: "solid" | "outlined" | "text";
   color?: "primary" | "secondary" | "danger";
   size?: "large" | "medium" | "small";
 };
 
+type ButtonProps = ComponentProps<"button"> & SharedProps;
+type LinkButtonProps = ComponentProps<typeof Link> & SharedProps;
+
 export default function ButtonComponent({
+  className,
+  variant,
+  color,
+  size,
+  children,
+  ...otherProps
+}: ButtonProps): ReactNode {
+  const classNames = useClassNames({ className, variant, color, size });
+
+  return (
+    <button className={classNames} {...otherProps}>
+      {children}
+    </button>
+  );
+}
+
+export function LinkButtonComponent({
+  className,
+  variant,
+  color,
+  size,
+  children,
+  ...otherProps
+}: LinkButtonProps): ReactNode {
+  const classNames = useClassNames({ className, variant, color, size });
+
+  return (
+    <Link className={classNames} {...otherProps}>
+      {children}
+    </Link>
+  );
+}
+
+function useClassNames({
   className,
   variant = "solid",
   color = "primary",
   size = "large",
-  children,
-  ...otherProps
-}: Props): ReactNode {
-  return (
-    <button
-      className={clsx(
-        styles.button,
-        styles[variant],
-        styles[size],
-        styles[color],
-        className,
-      )}
-      {...otherProps}
-    >
-      {children}
-    </button>
+}: SharedProps): string {
+  return clsx(
+    styles.button,
+    styles[variant],
+    styles[size],
+    styles[color],
+    className,
   );
 }
