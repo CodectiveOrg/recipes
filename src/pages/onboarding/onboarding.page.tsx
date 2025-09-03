9ac585e41780db629e7df39786a82af5c0e47d70
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-03 11:11:46 +0330

    [REFACTOR] onboarding

--- src/pages/onboarding/onboarding.page.tsx
+++ src/pages/onboarding/onboarding.page.tsx
@@ -1,35 +1,23 @@
 import type { ReactNode } from "react";
 
-import { useNavigate } from "react-router";
+import { Link } from "react-router";
 
-import image from "@/assets/images/foods.png";
-
-import ButtonComponent from "@/components/button/button.component";
 import TypographyComponent from "@/components/typography/typography.component";
 
 import styles from "./onboarding.module.css";
 
 export default function OnboardingPage(): ReactNode {
-  const navigate = useNavigate();
-
-  const handleGetStartedButtonClick = (): void => {
-    navigate("/sign-in");
-  };
-
   return (
     <div className={styles.onboarding}>
-      <header></header>
       <main>
-        <img src={image} alt="" />
+        <img src="/images/onboarding.png" alt="" />
         <div className={styles.writings}>
           <TypographyComponent variant="h1">Start Cooking</TypographyComponent>
           <TypographyComponent p variant="p1" color="text-secondary">
-            Let’s join our community to cook better food!
+            Let's join our community to cook better food!
           </TypographyComponent>
         </div>
-        <ButtonComponent onClick={handleGetStartedButtonClick}>
-          Get Started
-        </ButtonComponent>
+        <Link to="/sign-in">Get Started</Link>
       </main>
     </div>
   );
