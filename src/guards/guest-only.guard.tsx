fcec375b49b16eb20270e106139b9c08e1e4b0bb
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-03 10:46:57 +0330

    [ADD] loading component

--- src/guards/guest-only.guard.tsx
+++ src/guards/guest-only.guard.tsx
@@ -2,13 +2,15 @@ import type { ReactNode } from "react";
 
 import { Navigate, Outlet } from "react-router";
 
+import LoadingComponent from "@/components/loading/loading.component.tsx";
+
 import useVerifyQuery from "@/queries/use-verify.query.ts";
 
 export default function GuestOnlyGuard(): ReactNode {
   const { isPending, isError } = useVerifyQuery();
 
   if (isPending) {
-    return <div>Loading...</div>;
+    return <LoadingComponent />;
   }
 
   if (isError) {
