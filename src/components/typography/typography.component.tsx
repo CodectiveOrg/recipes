19866d731d8fb1686cb88c78f8056c20b7bf5631
zahra zare <103643765+zahrazare3099@users.noreply.github.com>
2025-09-03 09:58:19 +0330

    48 Featured Recipe Card (#19)
    * initial component
    
    * Implement component
    
    * fix eslint
    
    * [RENAME] card props
    
    * display recipe card component
    
    * remove unuseable import file
    
    * fix eslint
    
    * [REMOVE] images
    
    * [RENAME] featured recipe card
    
    * [ADD] more entities
    
    * [REFACTOR] tsx
    
    * [REFACTOR] css
    
    * [UPDATE] duration format
    
    * [ADD] overlay
    
    * [ADD] ellipsis
    
    * [REMOVE] svg formatter
    
    * [ADD] eol config
    
    ---------
    
    Co-authored-by: BijanProgrammer <bijaneisapour@gmail.com>

--- src/components/typography/typography.component.tsx
+++ src/components/typography/typography.component.tsx
@@ -10,7 +10,7 @@ type Props = PropsWithChildren<
     className?: string;
     style?: CSSProperties;
     variant: "h1" | "h2" | "h3" | "p1" | "p2" | "s";
-    color?: "primary" | "text" | "text-secondary";
+    color?: "inherit" | "primary" | "text" | "text-secondary";
     maxLines?: number;
   }
 >;
@@ -22,7 +22,7 @@ export default function TypographyComponent({
   className,
   style,
   variant,
-  color = "text",
+  color = "inherit",
   maxLines,
   children,
 }: Props): ReactNode {
